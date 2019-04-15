2dd560993fa3cb35784f7daa6aa0828c1984e8dc
Alex Lundquist <alex@alexludnquist.com>
2019-04-15 12:07:56 -0600

    Initial Commit

---
+++ ichiban/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ichiban
+//
+//  Created by Alex Lundquist on 4/15/19.
+//  Copyright © 2019 Alex Lundquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
